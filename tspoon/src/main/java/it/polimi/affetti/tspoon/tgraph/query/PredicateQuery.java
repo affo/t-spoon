59b2f961174f6d41c93ec27e8734b39f945578fd
Lorenzo Affetti <lorenzo.affetti@gmail.com>
2017-12-28 16:42:22 +0100

    SelectAll and SelectNone default predicates added

--- tspoon/src/main/java/it/polimi/affetti/tspoon/tgraph/query/PredicateQuery.java
+++ tspoon/src/main/java/it/polimi/affetti/tspoon/tgraph/query/PredicateQuery.java
@@ -7,7 +7,11 @@ import java.util.function.Predicate;
  * Created by affo on 02/08/17.
  */
 public class PredicateQuery<T> extends Query {
-    private QueryPredicate<T> predicate;
+    public final QueryPredicate<T> predicate;
+
+    public PredicateQuery() {
+        this.predicate = new SelectNone<>();
+    }
 
     public PredicateQuery(String nameSpace, QueryPredicate<T> predicate) {
         super(nameSpace);
@@ -24,5 +28,21 @@ public class PredicateQuery<T> extends Query {
     }
 
     public interface QueryPredicate<T> extends Predicate<T>, Serializable {
+    }
+
+    public static class SelectAll<T extends Object> implements QueryPredicate<T> {
+
+        @Override
+        public boolean test(T anObject) {
+            return true;
+        }
+    }
+
+    public static class SelectNone<T extends Object> implements QueryPredicate<T> {
+
+        @Override
+        public boolean test(T anObject) {
+            return false;
+        }
     }
 }
