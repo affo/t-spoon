52e673dd72e28bb26d9215b9d67e38215aba043e
Lorenzo Affetti <lorenzo.affetti@gmail.com>
2018-01-24 14:45:01 +0100

    launching eval for different strategies and isolation levels

--- launch_scripts/launch_evaluation.sh
+++ launch_scripts/launch_evaluation.sh
@@ -7,37 +7,65 @@ if [[ "$#" -lt 1 ]]; then
 fi
 
 export RESULTS_DIR=$RESULTS_DIR/$1
+BASE_RESULTS_DIR=$RESULTS_DIR
 
 rm -r $RESULTS_DIR
 mkdir -p $RESULTS_DIR
 
-echo; echo; echo;
-echo "Launching series..."
-sleep 2
-launch_suite_series_1tg
+opt_isolation=(0 1 2 3)
+pess_isolation=(3 4)
 
-echo; echo; echo;
-echo "Launching series (separated TG)..."
-sleep 2
-launch_suite_series_ntg
+for opt in true false; do
 
-echo; echo; echo;
-echo "Launching parallel"
-sleep 2
-launch_suite_parallel_1tg
+    export IS_OPTIMISTIC=$opt
 
-echo; echo; echo;
-echo "Launching parallel (separated TG)..."
-sleep 2
-launch_suite_parallel_ntg
+    isolation=()
+    strategy_name=""
+    if [[ $opt = true ]]; then
+        isolation=${opt_isolation[*]}
+        strategy_name=OPT
+    else
+        isolation=${pess_isolation[*]}
+        strategy_name=PESS
+    fi
 
-echo; echo; echo;
-echo "Launching keyspace..."
-sleep 2
-launch_suite_keyspace
+    for level in $isolation; do
 
-echo; echo; echo;
-echo "Launching querying..."
-sleep 2
-launch_suite_query
+        export ISOLATION=$level
+        export RESULTS_DIR=$BASE_RESULTS_DIR/$strategy_name/PL$level
+        mkdir -p $RESULTS_DIR
 
+        echo; echo ">>> Strategy: $strategy_name, Isolation level: $ISOLATION"
+        echo ">>> Saving results to $RESULTS_DIR"
+
+        echo; echo; echo;
+        echo "Launching series..."
+        sleep 2
+        launch_suite_series_1tg
+
+        echo; echo; echo;
+        echo "Launching series (separated TG)..."
+        sleep 2
+        launch_suite_series_ntg
+
+        echo; echo; echo;
+        echo "Launching parallel"
+        sleep 2
+        launch_suite_parallel_1tg
+
+        echo; echo; echo;
+        echo "Launching parallel (separated TG)..."
+        sleep 2
+        launch_suite_parallel_ntg
+
+        echo; echo; echo;
+        echo "Launching keyspace..."
+        sleep 2
+        launch_suite_keyspace
+
+        echo; echo; echo;
+        echo "Launching querying..."
+        sleep 2
+        launch_suite_query
+    done
+done
