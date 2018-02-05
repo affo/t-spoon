0b71c04db47e9b91ab8c8112d94c88d89fd1b09e
Lorenzo Affetti <lorenzo.affetti@gmail.com>
2018-02-05 13:04:08 +0100

    [query/performance] indexing for faster random query

--- tspoon/src/main/java/it/polimi/affetti/tspoon/tgraph/db/Shard.java
+++ tspoon/src/main/java/it/polimi/affetti/tspoon/tgraph/db/Shard.java
@@ -5,12 +5,13 @@ import it.polimi.affetti.tspoon.tgraph.Metadata;
 import it.polimi.affetti.tspoon.tgraph.query.*;
 import org.apache.log4j.Logger;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Random;
-import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /**
  * Created by affo on 19/12/17.
@@ -22,6 +23,7 @@ public class Shard<V> implements
     // I suppose that the type for keys is String. This assumption is coherent,
     // for instance, with Redis implementation: https://redis.io/topics/data-types-intro
     protected final Map<String, Object<V>> state;
+    protected final List<String> keySpaceIndex;
     private final int shardNumber, numberOfShards;
     private final boolean externalReadCommitted;
     private final ObjectFunction<V> objectFunction;
@@ -45,6 +47,7 @@ public class Shard<V> implements
         this.LOG = Logger.getLogger("Shard[" + shardNumber + "] - " + nameSpace);
         this.state = new ConcurrentHashMap<>();
         this.transactions = new ConcurrentHashMap<>();
+        this.keySpaceIndex = new ArrayList<>();
     }
 
     public void forceSerializableRead() {
@@ -54,7 +57,10 @@ public class Shard<V> implements
     // --------------------------------------- Transaction Execution and Completion ---------------------------------------
 
     protected synchronized Object<V> getObject(String key) {
-        return state.computeIfAbsent(key, k -> new Object<>(objectFunction));
+        return state.computeIfAbsent(key, k -> {
+            keySpaceIndex.add(k);
+            return new Object<>(objectFunction);
+        });
     }
 
     /**
@@ -134,23 +140,16 @@ public class Shard<V> implements
             return;
         }
 
-        Set<Integer> indexes;
+        Stream<Integer> indexes;
         if (noKeys > noKeysToQuery) {
             indexes = random.ints(0, noKeys).distinct().limit(noKeysToQuery)
-                    .boxed().collect(Collectors.toSet());
+                    .boxed();
         } else {
             // select *
-            indexes = IntStream.range(0, noKeysToQuery).boxed().collect(Collectors.toSet());
+            indexes = IntStream.range(0, keySpaceIndex.size()).boxed();
         }
 
-        int i = 0;
-        for (String key : state.keySet()) {
-            if (indexes.contains(i)) {
-                query.addKey(key);
-            }
-            i++;
-        }
-
+        indexes.forEach(index -> query.addKey(keySpaceIndex.get(index)));
         queryState(query);
     }
 
