553c4b44502647f2bdf0f8ce8b3f003f40d1cd2f
Lorenzo Affetti <lorenzo.affetti@gmail.com>
2018-05-03 17:23:22 +0200

    [fix] use full tid indexing for PL4

--- tspoon/src/main/java/it/polimi/affetti/tspoon/tgraph/twopc/TidForWatermarkingTransactionsIndex.java
+++ tspoon/src/main/java/it/polimi/affetti/tspoon/tgraph/twopc/TidForWatermarkingTransactionsIndex.java
@@ -1,6 +1,10 @@
 package it.polimi.affetti.tspoon.tgraph.twopc;
 
+import it.polimi.affetti.tspoon.common.OrderedElements;
 import it.polimi.affetti.tspoon.tgraph.Vote;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by affo on 04/08/17.
@@ -10,19 +14,32 @@ import it.polimi.affetti.tspoon.tgraph.Vote;
  * At (optimistic) PL4 isolation level, we use transaction ids for versioning.
  */
 public class TidForWatermarkingTransactionsIndex<T> extends StandardTransactionsIndex<T> {
+    private OrderedElements<Long> watermarks = new OrderedElements<>(wm -> wm);
+    private long watermark;
+
     public TidForWatermarkingTransactionsIndex(long startingPoint, int sourceParallelism, int sourceID) {
         super(startingPoint, sourceParallelism, sourceID);
+        this.watermark = startingPoint;
     }
 
     @Override
     public long updateWatermark(long timestamp, Vote vote) {
-        long tid = getTransactionId(timestamp);
         if (vote != Vote.REPLAY) {
-            // we use transaction ids for the watermark
-            return super.updateWatermark(tid, vote);
+            // no update if the vote is replay, because the tnx has not yet completed
+            long tid = getTransactionId(timestamp);
+            watermarks.addInOrder(tid);
+            List<Long> removed = watermarks.removeContiguousWith(watermark);
+
+            if (!removed.isEmpty()) {
+                watermark = Collections.max(removed);
+            }
         }
 
-        // no update
-        return getCurrentWatermark();
+        return watermark;
+    }
+
+    @Override
+    public long getCurrentWatermark() {
+        return watermark;
     }
 }
