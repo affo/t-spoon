3a5efdd38d7c6220d75ad1ec3d9faac2e2925573
Lorenzo Affetti <lorenzo.affetti@gmail.com>
2019-09-04 09:53:14 +0200

    script to plot volt results from csv file

--- launch_scripts/plot_volt.py
+++ launch_scripts/plot_volt.py
@@ -12,32 +12,56 @@ if __name__ == '__main__':
     fname = sys.argv[1]
     df = pd.read_csv(fname, names=['ts', 'percentage', 'tp', 'lat'])
     df = df.sort_values('percentage')
+    df = df.reset_index(drop=True)
+
+    # aggregate
+    grouped = df.groupby('percentage')
+    tp = pd.DataFrame(pd.concat([
+        pd.Series(name),
+        group.tp.describe().rename(name),
+        ]) for name, group in grouped)
+    tp.rename(columns={tp.columns[0]: 'var', 'mean': 'value'}, inplace=True)
+    lat = pd.DataFrame(pd.concat([
+        pd.Series(name),
+        group.lat.describe().rename(name),
+        ]) for name, group in grouped)
+    lat.rename(columns={lat.columns[0]: 'var', 'mean': 'value'}, inplace=True)
+
+    #print tp
+    #print lat
+    #sys.exit(1)
 
     # ------ throughput
     cmn.reset_colors_and_markers()
     fig, ax = plt.subplots()
-    for key, group in df.groupby('percentage'):
-        cmn.my_plot(group, ax, kind='line', x='percentage', y='tp', label=key)
+    #for key, group in df.groupby('percentage'):
+    #    cmn.my_plot(group, ax, kind='line', x='percentage', y='tp', label=key)
+    cmn.my_plot(tp, ax, kind='line', x='var', y='value')
 
-    ax.set_ylim((0, 15000))
+    #ax.set_ylim((0, 80000))
+    ax.set_xlim((-10, 110))
     ax.margins(y=0.1)
-    ax.set_ylabel(cmn.TP_LABEL)
+    ax.set_ylabel("Average throughput [Ktxn/s]")
     ax.set_xlabel('Single partition transactions percentage')
-    ax.set_xscale('log')
-    plt.gca().invert_xaxis()
+    ax.set_yscale('log')
+    #plt.gca().invert_yaxis()
+    ax.get_legend().remove()
     cmn.savefig('volt_tp', fig)
 
     # ------ latency
     cmn.reset_colors_and_markers()
 
     fig, ax = plt.subplots()
-    for key, group in df.groupby('percentage'):
-        cmn.my_plot(group, ax, kind='line', x='percentage', y='lat', label=key)
+    #for key, group in df.groupby('percentage'):
+    #    cmn.my_plot(group, ax, kind='line', x='percentage', y='lat', label=key)
+    cmn.my_plot(lat, ax, kind='line', x='var', y='value')
 
-    ax.set_ylim((0, 50))
+    #ax.set_ylim((0, 8))
+    ax.set_xlim((-10, 110))
     ax.margins(y=0.1)
-    ax.set_ylabel(cmn.LAT_LABEL)
+    ax.set_ylabel("Average latency [ms]")
     ax.set_xlabel('Single partition transactions percentage')
-    ax.set_xscale('log')
-    plt.gca().invert_xaxis()
+    ax.set_yscale('log')
+    #plt.gca().invert_yaxis()
+    ax.get_legend().remove()
     cmn.savefig('volt_lat', fig)
